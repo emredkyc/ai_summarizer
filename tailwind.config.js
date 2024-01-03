045cac0afef36de0cc32aefc0912e802124e93f8
emredkyc <emredkyc@hotmail.com>
2024-01-03 23:41:52 +0100

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
